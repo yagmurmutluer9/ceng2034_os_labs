60d5fcb6732dc8b562d39f2339c2980335cdfa6a
U160709040 <yagmurmutuler9@gmail.com>
2019-03-03 00:33:04 -0800

    initial commit

--- os_lab_0/1.py
+++ os_lab_0/1.py
@@ -3,10 +3,17 @@ import os.path, time
 #this is for printing files in the directory with their last modified time. 
 for root,  dirs, files in os.walk("."):
     for filename in files:
-        #print(filename)
-         print("Last modified: %s" % time.ctime(os.path.getmtime(filename)),filename)
+         print( "Last modified time of %s : %s" %  (filename, time.ctime(os.path.getmtime(filename))))
 
+
+print()
+print()
 # this is for finding files end with txt. 
+counter = 0
+
 for file in os.listdir("."):
     if file.endswith(".txt"):
-        print(os.path.join(".", file))
+        print(os.path.join(root, file))
+        counter+=1
+
+print('\ntotal number of ".txt" files found: %d' % counter)
