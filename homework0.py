5afdbcb42326f7ac43fcee905d577b593e92f61e
U160709040 <yagmurmutuler9@gmail.com>
2019-03-03 12:24:39 -0800

    I added new changes

---
+++ homework0.py
@@ -0,0 +1,34 @@
+import os.path, time
+from pathlib import Path
+
+
+#change path to home directory and create "os_lab_0" 
+home = str(Path.home())
+os.chdir(home)
+os.mkdir('os_lab_0')
+os.chdir('os_lab_0')
+
+
+#create  empty files 
+
+f=open('a.txt', "w+").close()
+f=open('b.txt', "w+").close()
+f=open('z.py', "w+").close()
+
+#this is for printing files in the directory with their last modified time. 
+for root,  dirs, files in os.walk("."):
+    for filename in files:
+         print( "Last modified time of %s : %s" %  (filename, time.ctime(os.path.getmtime(filename))))
+
+
+print()
+print()
+# this is for finding files end with txt. 
+counter = 0
+
+for file in os.listdir("."):
+    if file.endswith(".txt"):
+        print(os.path.join(root, file))
+        counter+=1
+
+print('\ntotal number of ".txt" files found: %d' % counter)
